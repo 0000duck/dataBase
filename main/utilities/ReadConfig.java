5bd6af4dfaeb4d42df05c4be7a4e28052d19481f
Christian Diwold <diw@keba.com>
2015-02-09 08:32:01 +0100

    Config File read path code cleanup

--- main/utilities/ReadConfig.java
+++ main/utilities/ReadConfig.java
@@ -5,33 +5,32 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-import utilities.CustomConstant;
+import utilities.ProjectParam;
 import utilities.LogFile;
 
 public class ReadConfig {
 
-	File configFile;
 	public boolean fileExists = false;
-	final static String fileName = "dataBase.config";
-	final static String rootPath = "RootPath:";
-	
+	final static String FILENAME = "dataBase.config";
+	final static String ROOT_PATH = "RootPath:";
+
 	public ReadConfig() {
 
-		configFile = new File(fileName);
-		if (doesExist()) {
+		File configFile = new File(FILENAME);
+		if (doesExist(configFile)) {
 			try {
-				readConfig();
+				readConfig(configFile);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		} else {
-			
+
 		}
 
 	}
 
-	private boolean doesExist() {
+	private boolean doesExist(File configFile) { // TODO maybe put into ReadConfig()
 		fileExists = configFile.exists();
 		if (fileExists) {
 			LogFile.write("Config File " + configFile + " found");
@@ -42,25 +41,22 @@ public class ReadConfig {
 
 	}
 
-	private void readConfig() throws IOException {
+	private void readConfig(File configFile) throws IOException {
 
 		BufferedReader lineReader = new BufferedReader(new FileReader(configFile));
 		String line = lineReader.readLine();
-		line.trim();
-		setDirectoryPath(line);
+		line = line.trim();
+		parseLine(line);
 		lineReader.close();
-
 	}
 
-	private void setDirectoryPath(String line) {
-		boolean directory;
-		directory = line.contains(rootPath);
-		if (directory) {
-			LogFile.write("Directory:" + line.substring(rootPath.length()));
-			CustomConstant.ROOT_PATH = line.substring(rootPath.length());
+	private void parseLine(String line) {
+		if (line.contains(ROOT_PATH)) {
+			String path = line.substring(ROOT_PATH.length());
+
+			ProjectParam.ROOT_PATH = path;
+			LogFile.write("Directory:" + path);
 		}
 
 	}
 }
-	
-	
