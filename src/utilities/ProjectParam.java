37f2182901ab39e82e06c980fa510d29b70212a7
Christian Diwold <diw@keba.com>
2015-09-25 07:21:33 +0200

    Version Release

--- src/utilities/ProjectParam.java
+++ src/utilities/ProjectParam.java
@@ -11,7 +11,7 @@ public final class ProjectParam {
 
 	
 	//VERSION--------------------------------------------------------------------------------------------
-	public static final String DataBaseVersion = "DataBase 1.1";
+	public static final String DataBaseVersion = "DataBase 1.2";
 	
 	//COLORS --------------------------------------------------------------------------------------------
 	public final static Color NO_MATCH_COL = new Color(255,140,140);
